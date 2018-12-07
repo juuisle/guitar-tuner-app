70953532545e00869e642fdaf69b3c1cc485c4b7
Duy Le-Dinh <henryledinhduy@gmail.com>
2018-12-07 10:44:45 +0200

    changed requiement file

--- raspery-app/ras.py
+++ raspery-app/ras.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python
 import requests
+import serial
+import datetime
+import os
+
 
 ROOT_URL = "http://127.0.0.1:5000/selected"
 
 r = requests.get(ROOT_URL)
-
 print r.text
+
+#os.chdir("/home/pi/python")
+#ser = serial.Serial("/dev/ttyUSB0",9600)
